76693ed11ca2584188f631d1e27f5c9199a92a5e
IevaSta <ieva@kontraforma.lt>
2022-06-11 14:32:22 +0300

    IIFE created && append tested

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
 import { file } from './lib/file.js'
 
 
-// const readStatus = await file.read('userssssss', 'petras.json');
-// console.log(readStatus);
+//IIFE = ifis
+(async () => {
 
-const userMaryte = {
-    name: 'Maryte',
-    age: 87,
-    isMarried: false,
-}
+    // const user = {
+    // }
 
-const createStatus = await file.create('users', 'betkas.json', userMaryte);
-console.log(createStatus);
+    // user.test = "Appended";
+    // const append = await file.append('users', 'test.json', user);
+    // console.log('Append:', append);
+
+    const readStatus1 = await file.read('users', 'petras.json');
+    console.log('Read1:', readStatus1);
+
+    const user = {
+        name: 'Petras',
+        age: 87,
+        isMarried: false,
+    }
+
+    const createStatus = await file.create('users', 'petras.json', user);
+    console.log('Create:', createStatus);
+
+    const readStatus2 = await file.read('users', 'petras.json');
+    console.log('Read2:', readStatus2);
+
+    user.isMarried = true;
+    const updateStatus = await file.update('users', "petras.json", user)
+    console.log('Update:', updateStatus);
+
+    const readStatus3 = await file.read('users', 'petras.json');
+    console.log('Read3:', readStatus3);
+
+    const deleteStatus = await file.delete('users', 'petras.json');
+    console.log('Delete:', deleteStatus);
+
+    const readStatus4 = await file.read('users', 'petras.json');
+    console.log('Read4:', readStatus4);
+
+})();
